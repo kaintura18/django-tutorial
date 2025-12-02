55d0074b3126ce6f2ba184bc3b2267204e4e22c8
kentura <kaintura.aditya@gmail.com>
2025-12-03 00:41:31 +0530

    added model(data table) --heronames and registered to admin

--- arty/farty/admin.py
+++ arty/farty/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import heroname
 
+admin.site.register(heroname)
 # Register your models here.
--- arty/farty/models.py
+++ arty/farty/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class heroname(models.Model):
+  herotype_choices = [('warrior', 'Warrior'), ('mage', 'Mage'), ('archer', 'Archer'),('healer', 'Healer')]
+  name = models.CharField(max_length=100)
+  image= models.ImageField(upload_to='heronames/')
+
+  age = models.IntegerField()
+  herotype = models.CharField(max_length=20, choices=herotype_choices )
+
+  def __str__(self):
+      return self.name
